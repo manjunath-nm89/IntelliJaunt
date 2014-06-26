f750490d1ffff0d830fa6488a90cfd8cf248bb08
Manju <manjunath.nm89@gmail.com>
2014-06-26 19:16:35 +0530

    Generic chnages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  attr_accessor :current_user
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
-  def logged_in?
-    !!current_user
-  end
+  ## methods provided by devise
+  ## current_user, user_signed_in?
 
 end
